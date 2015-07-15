e12057f881ebf3fbf5cc57b9568dd371aa9734b3
Eugenio Luna <geustodj@yahoo.com>
2015-07-15 16:18:42 -0500

    no avance
    no funciona ni una sola parte del codigo

---
+++ js/fnbuttons.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){    
+    $('#guardar').click(function(){      
+    
+    //funcion para captura en inputs
+    var nom = document.getElementById("nametxt").value;
+    var mail = document.getElementById("emailtxt").value;
+            
+        localStorage.setItem("subject", nom);
+        localStorage.setItem("identi", mail);
+        
+       document.getElementById("nametxt").value = "";
+       document.getElementById("emailtxt").value = "";
+        
+      
+    });
+});
+
+//funcion obtener
+$(document).ready(function(){    
+    $('#result').click(function() {      
+                       
+        //Obtener datos almacenados
+        var name = localStorage.getItem("subject");
+        var email = localStorage.getItem("identi");        
+                
+        //Mostrar datos almacenados      
+        document.getElementById("name").innerHTML= name;
+        document.getElementById("email").innerHTML= email;            
+        
+    });   
+});
